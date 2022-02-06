88f710e62016bfcfcc5afbf29b689ca2fdd12273
uzinok_HP <uzinok@yandex.ru>
2022-02-07 02:48:30 +0300

    правки для видео

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-// устанавливаем настройки
 const options = {
-	// родитель целевого элемента - область просмотра
-	root: null,
-	// отступ
-	rootMargin: '900px',
-	// процент пересечения
-	threshold: 0
-}
+  rootMargin: '500px'
+};
 
-// создаем наблюдатель
-const observer = new IntersectionObserver((entries, observer) => {
-	// для каждой записи-целевого элемента
-	entries.forEach(entry => {
-		// если элемент является наблюдаемым
-		if (entry.isIntersecting) {
-			const lazyIframe = entry.target;
-			// выводим информацию в консоль - проверка работоспособности наблюдателя
-			console.log(lazyIframe);
-			// меняем значение фтрибута что бы загрузить iframe
-			lazyIframe.src = 'https://yandex.ru/map-widget/v1/?um=constructor%3A818c095a301146ed081e9cc2e4a2d73cb5123c36ec0258a623082ec152a8697e&amp;source=constructor';
-			// прекращаем наблюдение
-			observer.unobserve(lazyIframe);
-		}
-	});
-}, options)
+var intersectionObserver = new IntersectionObserver(function (entries) {
+  // Если intersectionRatio равен 0, цель вне зоны видимости
+  // и нам не нужно ничего делать
+  if (entries[0].intersectionRatio <= 0) return;
 
-// следим за iframe
-const iframe = document.querySelector('iframe');
-observer.observe(iframe);
+  console.log(entries);
+  entries[0].target.src = entries[0].target.dataset.src;
+  this.unobserve(entries[0].target);
+
+  // loadItems(10);
+  console.log('Loaded new items');
+}, options);
+// начать наблюдение
+intersectionObserver.observe(document.querySelector('iframe'));
