82ee01db83710bb1c64c9b026b4ba351062ace86
uzinok <uzinok@yandex.ru>
2022-02-07 21:51:36 +0300

    записал видео 07.02.2022

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 const options = {
   rootMargin: '500px'
-};
+}
 
 var intersectionObserver = new IntersectionObserver(function (entries) {
-  // Если intersectionRatio равен 0, цель вне зоны видимости
-  // и нам не нужно ничего делать
+  // If intersectionRatio is 0, the target is out of view
+  // and we do not need to do anything.
   if (entries[0].intersectionRatio <= 0) return;
 
-  console.log(entries);
-  entries[0].target.src = entries[0].target.dataset.src;
-  this.unobserve(entries[0].target);
-
   // loadItems(10);
-  console.log('Loaded new items');
+  // console.dir(entries[0].target);
+  entries[0].target.src = entries[0].target.dataset.src;
 }, options);
-// начать наблюдение
+// start observing
 intersectionObserver.observe(document.querySelector('iframe'));
